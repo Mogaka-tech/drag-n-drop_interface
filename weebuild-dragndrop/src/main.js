ee1df6778ad3cb7f93170c964901f58e39d672d5
Mogakamo <mogaka.amo254@gmail.com>
2021-10-07 13:27:32 +0000

    vue-single-spa is set

--- weebuild-dragndrop/src/main.js
+++ weebuild-dragndrop/src/main.js
@@ -1,12 +1,34 @@
 import Vue from "vue";
+import singleSpaVue from "single-spa-vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+          /*
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+          */
+        },
+      });
+    },
+    router,
+    store,
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
