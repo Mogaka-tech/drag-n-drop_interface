6d07eaa43d62cbe6e4021326c3a3ee0e29b3179e
Mogakamo <mogaka.amo254@gmail.com>
2021-10-07 13:44:36 +0000

    finished setting the basics for the plugin

---
+++ weebuild-dragndrop/src/components/DragandDrop.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="drag-n-drop-wrapper">
+    <div class="dd-sidebar"><h1>Drag</h1></div>
+    <div class="dd-content"><h1>Drop</h1></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DragDrop",
+};
+</script>
+
+<style scoped>
+.drag-n-drop-wrapper {
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  grid-column-gap: 10px;
+  grid-row-gap: 6px;
+}
+
+.dd-sidebar {
+  border: 1px solid black;
+  grid-row-start: 1;
+  grid-row-end: 3;
+  height: 90vh;
+}
+.dd-content {
+  border: 1px solid black;
+  height: 90vh;
+}
+</style>
